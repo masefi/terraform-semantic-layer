60100d49f5c24dcddb0d460e5e73709fcd025882
mehdi-asefi <mehdi@operatives.ai>
2026-01-16 12:52:25 -0800

    chore(infra): enable required GCP APIs including Vertex AI

---
+++ services.tf
@@ -0,0 +1,17 @@
+locals {
+  services = [
+    "aiplatform.googleapis.com",      # Vertex AI
+    "bigquery.googleapis.com",        # BigQuery
+    "artifactregistry.googleapis.com", # Artifact Registry
+    "run.googleapis.com",             # Cloud Run
+    "iam.googleapis.com"              # IAM
+  ]
+}
+
+resource "google_project_service" "project_services" {
+  for_each = toset(local.services)
+  project  = var.project_id
+  service  = each.key
+
+  disable_on_destroy = false
+}
